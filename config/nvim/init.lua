2bd1b613002b94c4f6123140b10f83e8787cc9ad
Victor Li <victorli5674@yahoo.com>
2025-06-26 15:40:40 -0700

    remove autopairs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1093,6 +1093,24 @@ require("lazy").setup({
 				},
 			})
 
+			require("mini.pairs").setup({
+				opts = {
+					event = "VeryLazy",
+					opts = {
+						modes = { insert = true, command = true, terminal = false },
+						-- skip autopair when next character is one of these
+						skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+						-- skip autopair when the cursor is inside these treesitter nodes
+						skip_ts = { "string" },
+						-- skip autopair when next character is closing pair
+						-- and there are more closing pairs than opening pairs
+						skip_unbalanced = true,
+						-- better deal with markdown code blocks
+						markdown = true,
+					},
+				},
+			})
+
 			-- ... and there is more!
 			--  Check out: https://github.com/echasnovski/mini.nvim
 		end,
@@ -1199,29 +1217,6 @@ require("lazy").setup({
 		opts = {
 			enable_autocmd = false,
 		},
-	},
-
-	{ -- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help ibl`
-		main = "ibl",
-		opts = {},
-	},
-
-	-- Autopairs
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		-- Optional dependency
-		dependencies = { "hrsh7th/nvim-cmp" },
-		config = function()
-			require("nvim-autopairs").setup({})
-			-- If you want to automatically add `(` after selecting a function or method
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp = require("cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		end,
 	},
 
 	{
