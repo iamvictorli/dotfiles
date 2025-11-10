718826f15a53bf75b753ef8ecd143423974a0016
Victor Li <victorli5674@yahoo.com>
2025-11-09 23:42:00 -0800

    add <C-d> and <C-u> keymap

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -3,3 +3,6 @@
 -- Add any additional keymaps here
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "[Y]anks to system clipboard" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "stays on middle screen while <C-d>" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "stays on middle screen while <C-u>" })
