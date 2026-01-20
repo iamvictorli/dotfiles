46e13f32c52ca5996cab9cba747733f47c9b73ba
Victor Li <victorli5674@yahoo.com>
2026-01-20 11:51:49 -0800

    nvim tmux navigation

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,12 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "[Y]anks to system c
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "stays on middle screen while <C-d>" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "stays on middle screen while <C-u>" })
+
+-- nvim-tmux-navigation
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, { desc = "Navigate Left" })
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { desc = "Navigate Down" })
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { desc = "Navigate Up" })
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { desc = "Navigate Right" })
+vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive, { desc = "Navigate Last Active" })
+vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext, { desc = "Navigate Next" })
--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -54,4 +54,12 @@ return {
       },
     },
   },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      require("nvim-tmux-navigation").setup({
+        disable_when_zoomed = true,
+      })
+    end,
+  },
 }
